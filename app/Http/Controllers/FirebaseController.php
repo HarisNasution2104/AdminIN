58999974244ece15d9367aed98f6b00e8606d991
Haris Nasution <harisnasution2104@gmail.com>
2024-02-02 15:20:44 +0000

    Firebasecontroller

--- app/Http/Controllers/FirebaseController.php
+++ app/Http/Controllers/FirebaseController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Kreait\Firebase\Facades\Firebase;
 
 class FirebaseController extends Controller
 {
-    //
+    public function storeDataInFirebase()
+    {
+        $database = Firebase::database();
+
+        // Mengambil referensi ke koleksi
+        $collection = $database->collection('nama_koleksi');
+
+        // Menambahkan dokumen baru
+        $document = $collection->add([
+            'field1' => 'value1',
+            'field2' => 'value2',
+        ]);
+
+        return 'Data berhasil ditambahkan ke Firestore dengan ID: ' . $document->id();
+    }
 }
